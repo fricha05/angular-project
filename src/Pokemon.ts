ca0d680dc1dd8d94cc3af495d34d7726a2d8cc9a
Florian Richard <florianrichard96@gmail.com>
2020-03-30 16:06:46 +0200

    exercice 4

--- src/Pokemon.ts
+++ src/Pokemon.ts
@@ -1,10 +1,26 @@
 export class Pokemon {
     name: string;
+    level: number;
+    health: number;
+    attack: number;
+    defense: number;
     speed: number;
+    attackBasePower: number;
 
-    constructor(name: string, speed: number){
+    constructor(name: string, level: number, health: number, attack: number, defense: number, speed: number, attackBasePower: number){
         this.name = name;
         this.speed = speed;
+        this.attack = attack;
+        this.defense = defense;
+        this.level = level;
+        this.health = health;
+        this.attackBasePower = attackBasePower;
+    }
+
+    public attackEnnemy(ennemy: Pokemon): void{
+        const { defense } = ennemy;
+        let damage = Math.floor(Math.floor(Math.floor(2 * this.level / 5 + 2)* this.attack * this.attackBasePower / defense) / 50) + 2
+        ennemy.health = ennemy.health - damage;
     }
 }
 
--- src/pokemon.spec.ts
+++ src/pokemon.spec.ts
@@ -1,15 +1,31 @@
 import { Pokemon, getStartingPokemon } from './Pokemon'
 
-describe('Pokemon', () => {
+describe('Pokemon Turn', () => {
     it('should return Salameche', () => {
-        let carapuce = new Pokemon("Carapuce", 43);
-        let salameche = new Pokemon("Salameche", 65);
+        let carapuce = new Pokemon("Carapuce", 5, 25, 50, 50, 43, 90);
+        let salameche = new Pokemon("Salameche", 5, 22, 60, 40, 65, 110);
         expect(getStartingPokemon(salameche, carapuce)).toBe(salameche);
     })
 
     it('should return Pikachu', () => {
-        let bulbizarre = new Pokemon("Bulbizarre", 45);
-        let pikachu = new Pokemon("Pikachu", 90);
+        let bulbizarre = new Pokemon("Bulbizarre", 5, 24, 55, 45, 45, 95);
+        let pikachu = new Pokemon("Pikachu", 5, 20, 65, 35, 90, 115);
         expect(getStartingPokemon(bulbizarre, pikachu)).toBe(pikachu);
     })  
+})
+
+describe('Pokemon Attack', () => {
+    it('salameche health sould be 11', () => {
+        let carapuce = new Pokemon("Carapuce", 5, 25, 50, 50, 43, 90);
+        let salameche = new Pokemon("Salameche", 5, 22, 60, 40, 65, 110);
+        carapuce.attackEnnemy(salameche);
+        expect(salameche.health).toBe(11);
+    })
+
+    it('bulbizarre health sould be 9', () => {
+        let bulbizarre = new Pokemon("Bulbizarre", 5, 24, 55, 45, 45, 95);
+        let pikachu = new Pokemon("Pikachu", 5, 20, 65, 35, 90, 115);
+        pikachu.attackEnnemy(bulbizarre);
+        expect(bulbizarre.health).toBe(9);
+    })
 })
